96afd188228a0f7c11adfce61c3a7b9bc0eada0b
slabon <slabontec@gmail.com>
2017-04-17 01:29:30 +0200

    Update bitcoinTicker.php

--- bitcoinTicker.php
+++ bitcoinTicker.php
@@ -2,61 +2,18 @@
 
 function coindeskBPI(){
 	$data = json_decode(getResource('http://api.coindesk.com/v1/bpi/currentprice.json'),'TRUE');
-	return $data['bpi']['USD']['rate'];
-}
-
-function btcChina(){
-	$data = json_decode(getResource('https://data.btcchina.com/data/ticker'),'TRUE');
-	return $data['ticker']['last'];
-}
-
-function OKCoin(){
-	$data = json_decode(getResource('https://www.okcoin.com/api/ticker.do'),'TRUE');
-	return $data['ticker']['last'];
-}
-
-function bitfinex(){
-	$data = json_decode(getResource('https://api.bitfinex.com/v1/ticker/btcusd'),'TRUE');
-	return $data['last_price'];
-}
-
-function coinbase(){
-#appears to update once per minute.
-	$data = json_decode(getResource('https://coinbase.com/api/v1/currencies/exchange_rates'),'TRUE');
-	return $data['btc_to_usd'];
-}
-
-function mtGox(){
-	$data = json_decode(getResource('http://data.mtgox.com/api/2/BTCUSD/money/ticker_fast'),'TRUE');
-	return $data['data']['last']['value'];
-}
-
-function btce(){
-	$data = json_decode(getResource('https://btc-e.com/api/2/btc_usd/ticker'),'TRUE');
-	return $data['ticker']['last'];
+	return $data['bpi']['EUR']['rate'];
 }
 
 function bitStamp(){
-	$data = json_decode(getResource('https://www.bitstamp.net/api/ticker/'),'TRUE');
+	$data = json_decode(getResource('https://www.bitstamp.net/api/v2/ticker/btceur/'),'TRUE');
 	return $data['last'];
-}
-
-function campBX(){
-	#CampBx Gives a 403 Forbidden error if not supplying a user agent string,
-	$data = json_decode(getResource('http://campbx.com/api/xticker.php'),'TRUE');
-	return $data['Last Trade'];
 }
 
 function kraken(){
 	$data = json_decode(getResource('https://api.kraken.com/0/public/Ticker?pair=XBTUSD'),'TRUE');
-	return $data['result']['XXBTZUSD']['c'][0];
+	return $data['result']['XXBTZEUR']['c'][0];
 }
-
-function crypto_trade(){
-	$data = json_decode(getResource('https://crypto-trade.com/api/1/ticker/btc_usd'),'TRUE');
-	return $data['data']['last'];
-}
-
 
 function getResource($url){
 	$ch = curl_init();
